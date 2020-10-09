2cc4155378556c7245752bee3dbc70343fa57722
Josephshitandi <jsphshtnd@gmail.com>
2020-10-09 22:27:32 +0300

    Add test_init method that checks if object is initialized proprly

--- test.py
+++ test.py
@@ -10,3 +10,19 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
+        
+        self.new_user = User('Joseph', '123456')
+        
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.user_name,"Joseph")
+        self.assertEqual(self.new_user.password,"123456")
+        
+        
+        
+        
+if __name__ == '__main__':
+    unittest.main()
