e5af82bf59af21299f551b60b05d0541aca8b415
Josephshitandi <jsphshtnd@gmail.com>
2020-10-09 20:30:27 +0300

    Add a class user and initialized the instances

---
+++ User.py
@@ -0,0 +1,8 @@
+class User:
+    '''
+    class that allows user to generate user account
+    '''
+    def __init__(self,user_name,password):
+        self.user_name = user_name
+        self.password = password
+    
