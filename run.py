39be315f44bf0d03e5519ebe021110f976ab8daf
Josephshitandi <jsphshtnd@gmail.com>
2020-10-10 18:55:30 +0300

    Add a function that fetchs user input and create account

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def save_user(user):
     '''
     User.save_user
     
-def veryfy_user(user_name,password):
+def verify_user(user_name,password):
     '''
     Function that veryfies the existing user
     
@@ -72,13 +72,74 @@ def display_credential(credentials):
     return Credentials.display_credentials()
 
 def main():
-    print(' ')
+    # print('')
     print('Hello! Welcome to password Locker')
-    
+    while True:
+        print("--"*50)
+        print('\n')
+        
+        print('Use these short codes: \n ca - Create an Account \n lg - Login \n ex - Exit')
+        print('\n')
+        short_code = input().lower() 
+        if short_code == 'ex':
+            break 
+        
+        elif short_code == 'ca':
+            print("-"*20)
+            print('\n')
+            print('Create a new account:')
+            print('\n')
+
+            
+            user_name = input('Enter your User name - ')
+            password = input('Enter your password - ')
+            save_user(create_user(user_name, password))
+            print(" ")
+            print(
+                f'New Account Created for: {user_name} using password: {password}')
+            print('\n')
+
+        elif short_code == 'lg':
+            print("--"*50)
+            print('\n')
+            print('Enter your account details to login:')
+            print('\n')
+
+            username = input('Enter your user name - ')
+            password = str(input('Enter your password - '))
+            user_exists = verify_user(username,password)
+            if user_exists == username:
+                print('\n')
+                print(f'Welcome {username}. Please select a short code to continue.')
+                print(' ')
+
+                while True:
+                    print("-"*30)
+                    print('Our short codes: \n cc-Create a Credential \n sc-Show Credentials \n fc- Find a Credential  \n ex-Exit')
+                    print('\n')
+                    short_code = input('Enter a choice: ').lower()
+                    print("-"*10)
 
 
+                    if short_code == 'ex':
+                        print(" ")
+                        print(f'Thank you for using Password Lock. Goodbye {username}')
+                        break
 
+                    elif short_code == 'cc':
+                        print('\n')
+                        print('Enter your new credentials:')
+                        print('\n')  
+                        account_name = input('Enter your account name - ')
 
+                        while True:
+                            print('\n')
+                            print("-"*20)
+                            print('Please select an option for creating a password: \n ep - enter your password \n gp - generate a password \n ex - exit')
+                            choice = input('Enter an option: ').lower()
+                            print("-"*10)
+                            
+                            
 
 
 
